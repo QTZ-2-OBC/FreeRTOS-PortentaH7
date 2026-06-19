e1c566830cf8f949e83d2fd9cb0c31f8491865cc
ElrohirGT <elrohirgt@gmail.com>
2026-06-19 15:42:54 -0600

    feat: Added CM4 specific debug implementation

---
+++ CM4/debug.c
@@ -0,0 +1,17 @@
+#include "usart.h"
+#include <debug.h>
+#include <string.h>
+
+// NOTE: Same buffer defined on debug.c
+// This is an implementation detail, this buffer should no be modified directly
+// under normal circumstances But we need it here since QTZ_Debug_Print needs
+// to know what to print!
+extern uint8_t *__DEBUG_INNER_BUFFER[QTZ_DEBUG_CAPACITY];
+const uint32_t QTZ_DEBUG_MAX_TIMEOUT = 0xFFFFFFFFUL;
+
+void QTZ_Debug_Init() { MX_UART7_Init(); }
+void QTZ_Debug_Print() {
+  uint16_t size = strlen((char *)__DEBUG_INNER_BUFFER);
+  HAL_UART_Transmit(&huart7, (uint8_t *)__DEBUG_INNER_BUFFER, size,
+                    QTZ_DEBUG_MAX_TIMEOUT);
+}
