9de36b2b8bf0308565b5017e8a3040bb96648745
ElrohirGT <elrohirgt@gmail.com>
2026-07-06 16:09:37 -0600

    feat: Added Debug conditional implementation

--- CM4/debug.c
+++ CM4/debug.c
@@ -2,16 +2,24 @@
 #include <debug.h>
 #include <string.h>
 
+#ifdef QTZ_DEBUG
 // NOTE: Same buffer defined on debug.c
 // This is an implementation detail, this buffer should no be modified directly
 // under normal circumstances But we need it here since QTZ_Debug_Print needs
 // to know what to print!
 extern uint8_t *__DEBUG_INNER_BUFFER[QTZ_DEBUG_CAPACITY];
 const uint32_t QTZ_DEBUG_MAX_TIMEOUT = 0xFFFFFFFFUL;
+#endif
 
-void QTZ_Debug_Init() { MX_USART6_UART_Init(); }
+void QTZ_Debug_Init() {
+#ifdef QTZ_DEBUG
+  MX_USART6_UART_Init();
+#endif
+}
 void QTZ_Debug_Print() {
+#ifdef QTZ_DEBUG
   uint16_t size = strlen((char *)__DEBUG_INNER_BUFFER);
   HAL_UART_Transmit(&huart6, (uint8_t *)__DEBUG_INNER_BUFFER, size,
                     QTZ_DEBUG_MAX_TIMEOUT);
+#endif
 }
--- Common/debug.c
+++ Common/debug.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#ifdef QTZ_DEBUG
 uint8_t __DEBUG_INNER_BUFFER[QTZ_DEBUG_CAPACITY];
 
 #define QTZ_LOG_WITH_PREFIX(prefix)                                            \
@@ -19,14 +20,27 @@ uint8_t __DEBUG_INNER_BUFFER[QTZ_DEBUG_CAPACITY];
     va_end(args);                                                              \
     QTZ_Debug_Print();                                                         \
   }
+#endif
 
 // NOTE: Make sure every prefix is the same length!
 // If you ever change the length of all prefixes then update the
 // QTZ_LOG_WITH_PREFIX accordingly!
 
 // Logs a debug message with the default priority
-void QTZ_Debug_Log(const char *msg, ...) { QTZ_LOG_WITH_PREFIX("LOG: "); }
+void QTZ_Debug_Log(const char *msg, ...) {
+#ifdef QTZ_DEBUG
+  QTZ_LOG_WITH_PREFIX("LOG: ");
+#endif
+}
 // Logs a warning message with a warning priority
-void QTZ_Debug_Warning(const char *msg, ...) { QTZ_LOG_WITH_PREFIX("WAR: "); }
+void QTZ_Debug_Warning(const char *msg, ...) {
+#ifdef QTZ_DEBUG
+  QTZ_LOG_WITH_PREFIX("WAR: ");
+#endif
+}
 // Logs an error message with the highest priority
-void QTZ_Debug_Error(const char *msg, ...) { QTZ_LOG_WITH_PREFIX("ERR: "); }
+void QTZ_Debug_Error(const char *msg, ...) {
+#ifdef QTZ_DEBUG
+  QTZ_LOG_WITH_PREFIX("ERR: ");
+#endif
+}
